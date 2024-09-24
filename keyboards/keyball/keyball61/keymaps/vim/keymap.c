5fc34cc0e68c97b3db83b9d9ff5a37a651173538
Hellmy <maoddoy@hotmail.com>
2024-09-24 09:52:46 +0200

    Update keymap.c

--- keyboards/keyball/keyball61/keymaps/vim/keymap.c
+++ keyboards/keyball/keyball61/keymaps/vim/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P      , KC_ENT  ,
     KC_BSLS   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT ,
     MO(1)    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH   , MO(1)    ,
-    KC_LCTL  , KC_LCTL  , KC_LGUI  , KC_LALT   , KC_BTN1 ,LT(2,KC_SPC),LT(3,KC_BTN2)  ,           PB_2    , LT(2,KC_SPC)   ,_______   ,_______   , _______  , KC_MINS  , KC_EQL
+    KC_LCTL  , KC_LCTL  , KC_LGUI  , KC_LALT   , KC_BTN1 ,LT(2,KC_SPC),LT(3,KC_BTN2)  ,           KC_BTN1  , LT(2,KC_SPC)   ,_______   ,_______   , _______  , KC_MINS  , KC_EQL
   ),
 
 
@@ -79,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_universal(
     KC_GRV , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT , _______  , KC_UP     , RALT(KC_5)    , KC_9     , _______  ,                                  _______  , A(KC_UP)  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
+    SSNP_VRT , _______  , KC_UP     , RALT(KC_5)    , KC_9     , _______  ,                                  _______  , A(KC_UP)  , KC_UP    , KC_MINS  , KC_EQL  , KC_F12   ,
     SSNP_HOR , KC_LEFT  , KC_DOWN  , KC_RGHT     , SSNP_FRE   ,KC_VOLD   ,                                  KC_PGUP  , A(KC_DOWN)  , A(KC_UP)  , KC_BTN2  , KC_BTN3  , _______  ,
     KC_LSFT   , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , KC_MUTE  , _______  , _______  , RALT(KC_S) , KC_RSFT     ,
-    TG(2)    , KC_LCTL  , KC_LGUI  , KC_LALT    , A(KC_LEFT) , _______  , A(KC_RGHT)  ,             PB_1  , _______  , _______  , _______  , _______  , KC_VOLD  , KC_VOLU
+    TG(2)    , KC_LCTL  , KC_LGUI  , KC_LALT    , A(KC_LEFT) , _______  , A(KC_RGHT)  ,             _______  , _______  , _______  , _______  , _______  , KC_VOLD  , KC_VOLU
   ),
 
   [3] = LAYOUT_universal(
