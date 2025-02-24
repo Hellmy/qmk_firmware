771689093c07882e6c026a7e4cf4d8f2cc39f750
Manuel <manuel@zeunerds.de>
2025-02-24 11:33:06 +0100

    reboot

--- keyboards/keyball/keyball61/keymaps/vim/keymap.c
+++ keyboards/keyball/keyball61/keymaps/vim/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_MOD  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,                                  DM_PLY1  , DM_PLY2  , RGB_M_T  , RGB_M_TW , _______  , DM_RSTP  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
     _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EE_CLR   ,            EE_CLR   , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
-    QK_BOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
+    QK_REBOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
   ),
 
   [4] = LAYOUT_universal(
