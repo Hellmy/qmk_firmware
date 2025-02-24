b17a57f5fa722266a1fe875e6879762a8877a324
Manuel <manuel@zeunerds.de>
2025-02-24 13:26:44 +0100

    wait and shif

--- keyboards/keyball/keyball61/keymaps/vim/config.h
+++ keyboards/keyball/keyball61/keymaps/vim/config.h
@@ -44,5 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VIM_G_MOTIONS
 #define AUTO_SHIFT_TIMEOUT 150
 #define NO_AUTO_SHIFT_SPECIAL
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define DYNAMIC_MACRO_DELAY 50
 
 
